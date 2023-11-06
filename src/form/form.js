87f2ecf917f3df4321185f4e3a2a4cf25ff1089c
Lorenzo <lorenzo-a@live.fr>
2023-11-06 19:45:41 +0100

    first commit

---
+++ src/form/form.js
@@ -0,0 +1,85 @@
+import "../assets/styles/style.scss";
+import "./form.scss";
+
+let errors = [];
+
+const form = document.querySelector("form");
+const errorList = document.getElementById("errors");
+const cancelBtn = document.querySelector(".btn-secondary");
+
+const params = new URL(location.href);
+const articleId = params.searchParams.get("id");
+
+const initiateForm = async () => {
+    const params = new URL(location.href);
+    const articleId = params.searchParams.get("id");
+
+    if (articleId) {
+        const response = await fetch(`https://restapi.fr/api/cda_lorenzo/${articleId}`);
+        const article = await response.json();
+        fillForm(article);
+    }
+}
+
+initiateForm();
+
+const fillForm = (article) => {
+    const formFields = form.querySelectorAll("input, select, textarea");
+    formFields.forEach(field => {
+        field.value = article[field.name];
+    })
+}
+
+cancelBtn.addEventListener("click", event => {
+    location.assign("./index.html");
+});
+
+form.addEventListener("submit", async event => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const entries = formData.entries();
+    const article = Object.fromEntries(entries);
+
+    if (formIsValid(article)) {
+        const json = JSON.stringify(article);
+
+        try {
+            const response = await fetch("https://restapi.fr/api/cda_lorenzo", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: json
+            })
+
+            if(response.status < 300) {
+                location.assign("./index.html")
+            }
+            const data = await response.json();
+            form.reset();
+        } catch (error) {
+            console.error(error);
+        }
+    }
+});
+
+const formIsValid = (article) => {
+    if (!article.title || !article.title || !article.image || !article.content) {
+        errors.push("Vous devez renseigner tous les champs");
+    }
+
+    if (article.content.length > 1000) {
+        errors.push("Le contenu ne doit pas dépasser 1000 caractères");
+    }
+
+    if (errors.length) {
+        let errorHTML = "";
+        errors.forEach(error => {
+            errorHTML += `<li>${error}</li>`
+        });
+        errorList.innerHTML = errorHTML;
+        errors = [];
+        return false;
+    } else {
+        errorList.innerHTML = "";
+        return true;
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,78 @@
+import "./style.scss";
+
+const articlesContainer = document.querySelector(".articles-container");
+
+const fetchArticles = async () => {
+    try {
+        const response = await fetch("https://restapi.fr/api/cda_lorenzo");
+        let articles = await response.json();
+        if (!(articles instanceof Array)) {
+            articles = [articles]
+        }
+        createArticlesDOM(articles);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+fetchArticles();
+
+const createArticlesDOM = (articles) => {
+    const articlesDOM = articles.map(article => {
+        const articleNode = document.createElement("div");
+        articleNode.classList.add("article");
+        articleNode.innerHTML = `
+         <img src="${article.image ? article.image : 'assets/images/unsplash.jpg'}" alt="Profile image">
+         <h2>${article.title}</h2>
+         <p class="article-author">${article.author} - <span>${
+            new Date(article.createdAt).toLocaleDateString("fr-FR", {
+                weekday: "long",
+                day: "2-digit",
+                month: "long",
+                year: "numeric",
+                hour: "2-digit",
+                minute: "2-digit"
+            })
+        }</span></p> 
+                <p class="article-content">${article.content}</p>
+                <div class="article-actions">
+                    <button class="btn btn-primary" data-id=${article._id}>Éditer</button>
+                    <button class="btn btn-danger" data-id=${article._id}>Supprimer</button>
+                </div>
+        `
+        return articleNode;
+    });
+
+    articlesContainer.innerHTML = "";
+    articlesContainer.append(...articlesDOM);
+
+    const allDeleteBtns = articlesContainer.querySelectorAll(".btn-danger");
+    const allEditBtns = articlesContainer.querySelectorAll(".btn-primary");
+
+    allDeleteBtns.forEach(btn => {
+        btn.addEventListener("click", async event => {
+            try {
+                const articleId = event.target.dataset.id;
+                const response = await fetch(`https://restapi.fr/api/cda_lorenzo/${articleId}`, {
+                    method: "DELETE"
+                })
+                const data = await response.json();
+                fetchArticles();
+            } catch (error) {
+                console.error(error);
+            }
+        })
+    })
+
+    allEditBtns.forEach(btn => {
+        btn.addEventListener("click", event => {
+            try {
+                const articleId = event.target.dataset.id;
+                location.assign(`/form.html?id=${articleId}`);
+            } catch (error) {
+                console.error(error);
+            }
+        })
+    })
+
+}
